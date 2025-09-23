4e98504c51c3e3191eaa16183a32721b8fca6764
Ajay7006-mca <ajay7006.mca25@chitkara.edu.in>
2025-09-23 22:09:24 +0530

    initial method

---
+++ OROM.java
@@ -0,0 +1,64 @@
+/*Description: Create a multilevel hierarchy to manage orders, where Order is the base class, ShippedOrder is a subclass, and DeliveredOrder extends ShippedOrder.
+Tasks:
+Define a base class Order with common attributes like orderId and orderDate.
+Create a subclass ShippedOrder with additional attributes like trackingNumber.
+Create another subclass DeliveredOrder extending ShippedOrder, adding a deliveryDate attribute.
+Implement a method getOrderStatus() to return the current order status based on the class level.
+*/
+package pillars.Inheritance;
+class Order{
+    private int orderId;
+    private String orderDate;
+
+    public Order(int orderId, String orderDate) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+}
+
+class ShippedOrder extends Order {
+    private String trackingNumber;
+
+    public ShippedOrder(int orderId, String orderDate, String trackingNumber) {
+        super(orderId, orderDate);
+        this.trackingNumber = trackingNumber;
+    }
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+}
+
+class DeliveredOrder extends ShippedOrder {
+    private String deliveryDate;
+
+    public DeliveredOrder(int orderId, String orderDate, String trackingNumber, String deliveryDate) {
+        super(orderId, orderDate, trackingNumber);
+        this.deliveryDate = deliveryDate;
+    }
+
+    public String getDeliveryDate() {
+        return deliveryDate;
+    }
+    void display(){
+        System.out.println(getOrderId());
+        System.out.println(getOrderDate());
+        System.out.println(getTrackingNumber());
+        System.out.println(deliveryDate);
+    }
+}
+
+public class OROM {
+    public static void main(String[] args) {
+        DeliveredOrder order = new DeliveredOrder(1, "2023-10-01", "TRACK123", "2025-10-05");
+        order.display();
+    }
+}
